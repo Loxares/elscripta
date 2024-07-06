7cea99302ddb8965e63528b300fa8650b50175c2
Loxares <diarduro66@gmail.com>
2024-07-06 17:50:45 +0200

    Add files via upload

---
+++ elscripta.py
@@ -0,0 +1,62 @@
+import time
+
+choice = ""
+
+while choice.lower() != "n":
+    print("                                                                     \r\n                                                                                          \r\n                                                                                          \r\n                                        ~!       ^7:                                      \r\n                                     .7GB~       .Y#5~                                    \r\n                                    ~G&B:          J&#Y.                                  \r\n                             .^    7#&#~            Y&&G^   ^!.                           \r\n                           .?BJ   ?##&J             :B#&B~  :BB?.                         \r\n                          ~G&B:  7&##B.              ?&##B^  ?&&G^                        \r\n                         7##&5  ^###&Y               :###&P  ^##&#~                       \r\n                        !###&?  5&##&7                G###&7 .G###B^                      \r\n                       .B###&! ^#####~                5&##&P  P###&P                      \r\n                       7&###&! ?&###&!                G#####^ G####&~                     \r\n                       Y&###&J ?#####G^             :Y####GP:.B####&?                     \r\n                       5#####B. .^P####G5Y55PPPPPPPPB####7   7#####&?                     \r\n                       Y&####&Y. .5#####&&&##############J:.!####&&&~  ::                 \r\n                   !Y!.~#GG####G5G########################BB####5JYY:JGP^                 \r\n                    ?&B7:  ^B#########&&#####PP######&&########G.  !B&G.                  \r\n                     Y&&5!~Y########BPJ!^5&&5  P#&?^!?YB###&####GYP###^                   \r\n                     ~&##&&&#&#BP###P:   Y&#BYY##&?   ~B#&GJPB&&&&####:                   \r\n                     ?&###&#GJ~. ~B&#BJ~ !###&####~.!P##&P.  :!YB&####5:                  \r\n                   ^Y#####5~.     ^G&#&#GP########PB&##&Y.      .7G####BY!:               \r\n            ..:^!JP#&###5^         .?B&#############&&G!           J####&&#G5Y?~          \r\n            !5G#&&&&####Y!^.         :7PB#&&&&&&&&#B5!.        :~?5B###&##BG5?~.          \r\n              .^!?YPG#&#&&#BPY7~:       .^!7????7!^.      :~?5GB#&#&&BP?~:.               \r\n                    .^?P#####&&#BG5?!^.             :^!?5G##&&#####P!:                    \r\n                        ^5####GB&#&&&#BGP555555555PGB#&&&##&GY####J                       \r\n                         .B##Y.:P&####&&&&&&&&&&&&&#######&5. 7##B.                       \r\n                          Y&#:  :B###5!~~!JG&##&BJ!^^^J###B:  :#&P                        \r\n                          J&#J:. ?&#G.     .J##5:      Y&&Y :!5#&Y                        \r\n                          ?&###Y !&#P        PB.       Y&&J !&##&J                        \r\n                          ~&##&? !&##?       JG       7##&J ^###&7                        \r\n                          .B##&7 ~####J.     JP     .?###&? ~####:                        \r\n                           7&#&7 :####&P.    JB     J&###&7 ^##&?                         \r\n                            Y&&J .G####B.    YB.    5&####^ ~&&5                          \r\n                             J&B. Y&##&P     5#:    7&###B. Y&Y.                          \r\n                              J&J ~&##&?    .B&7    :B##&Y !&J                            \r\n                               !G! 5&##^    !&#G.    Y&#B:.P!                             \r\n                                .^ :G&5     P##&?    ^##~ ..                              \r\n                                    :P^    7&###B:    ?!                                  \r\n                                          :B####&5                                        \r\n                                          !#####&G.                                       \r\n                                           ~B##&P:                                        \r\n                                            ^B&P.                                         \r\n                                             !B:                                          \r\n                                              :                                     ")
+    print("  _____ _ ____            _       _        \r\n | ____| / ___|  ___ _ __(_)_ __ | |_ __ _ \r\n |  _| | \\___ \\ / __| '__| | '_ \\| __/ _` |\r\n | |___| |___) | (__| |  | | |_) | || (_| |\r\n |_____|_|____/ \\___|_|  |_| .__/ \\__\\__,_|\r\n                           |_|             ")
+    print("Choose one:")
+    print("[1]|> Write script")
+    print("[2]|> Make IP Tracking urls")
+    print("[3]|> Update ElScripta")
+    print("[4]|> Exit")
+
+    option = int(input("ElScript_install|>|/: "))
+
+    # Perform actions based on the selected option
+    if option == 1:
+        print("Write script[20+]Max:")
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+        input()
+    elif option == 2:
+        print("Please enter a random URL[Example:https://get-free-followers.com:")
+        input()
+        print("Please enter a port[default 5555]:")
+        input()
+        print("Sent this URL to your victim to send u need to do this:[https://*youre-url*:*Youre port that u wrote*")
+        print("Example:https://get-free-followers.com:5555")
+        print("Press enter to continue..")
+        input()
+        print("Waiting for IPS..")
+        input()
+        input()
+        input()
+    elif option == 3:
+        print("Updating please wait..")
+        time.sleep(30)
+        print("Updated!!")
+    elif option == 4:
+        break
+    else:
+        print("Invalid option")
+
+    choice = input("Do you want to continue? (y/n): ")
